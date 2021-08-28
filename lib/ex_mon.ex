5f9c77d24caff4aafa0d13f96c277135498f5cb8
William <w.f.l@hotmail.com>
2021-08-28 19:10:18 -0300

    Criando a logica dos ataques, atualizando a pontuaçao e refatorando as mensagens

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -14,7 +14,7 @@ defmodule ExMon do
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
 
-    Status.print_round_message()
+    Status.print_round_message(Game.info())
   end
 
   def make_move(move) do
@@ -30,5 +30,7 @@ defmodule ExMon do
       :move_heal -> "realiza_cura"
       move -> Actions.attack(move)
     end
+
+    Status.print_round_message(Game.info())
   end
 end
