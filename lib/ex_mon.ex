5e1cc19101dc89f9059c88df74d88840ba77d719
William <w.f.l@hotmail.com>
2021-08-30 22:09:33 -0300

    finalizando o jogo com tratamentos de erro e automatizando o turno do computador

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -4,6 +4,7 @@ defmodule ExMon do
   @moduledoc false
 
   @computer_name "PC"
+  @computer_moves [:move_avg, :move_rnd, :move_heal]
 
   def create_player(name, move_rnd, move_avg, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
@@ -18,9 +19,19 @@ defmodule ExMon do
   end
 
   def make_move(move) do
+    Game.info()
+    |> Map.get(:status)
+    |> handle_status(move)
+  end
+
+  defp handle_status(:game_over, _move), do: Status.print_round_message(Game.info())
+
+  defp handle_status(_other, move) do
     move
     |> Actions.fetch_move()
     |> do_move()
+
+    computer_move(Game.info())
   end
 
   defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
@@ -33,4 +44,11 @@ defmodule ExMon do
 
     Status.print_round_message(Game.info())
   end
+
+  defp computer_move(%{turn: :computer, status: :continue}) do
+    move = {:ok, Enum.random(@computer_moves)}
+    do_move(move)
+  end
+
+  defp computer_move(_), do: :ok
 end
