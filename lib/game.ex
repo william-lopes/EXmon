f21950cc2a58e7ea8cf6d962b2eb97ad80a8dcc4
William <w.f.l@hotmail.com>
2021-08-31 22:48:05 -0300

    formatando o codigo do projeto

--- lib/game.ex
+++ lib/game.ex
@@ -23,10 +23,10 @@ defmodule ExMon.Game do
   def fetch_player(player), do: Map.get(info(), player)
 
   defp update_game_status(
-      %{player: %Player{life: player_life}, computer: %Player{life: computer_life}} = state
-    )
-    when player_life == 0 or computer_life == 0,
-    do: Map.put(state, :status, :game_over)
+         %{player: %Player{life: player_life}, computer: %Player{life: computer_life}} = state
+       )
+       when player_life == 0 or computer_life == 0,
+       do: Map.put(state, :status, :game_over)
 
   defp update_game_status(state) do
     state
--- lib/game/actions/heal.ex
+++ lib/game/actions/heal.ex
@@ -1,34 +1,34 @@
 defmodule ExMon.Game.Actions.Heal do
-    alias ExMon.Game
-    alias ExMon.Game.Status
+  alias ExMon.Game
+  alias ExMon.Game.Status
 
-    @heal_power 18..25
-    
-    def heal_life(player) do
-        player
-        |> Game.fetch_player()
-        |> Map.get(:life)
-        |> calculate_total_life()
-        |> set_life(player)
-    end
+  @heal_power 18..25
 
-    defp calculate_total_life(life), do: Enum.random(@heal_power) + life
+  def heal_life(player) do
+    player
+    |> Game.fetch_player()
+    |> Map.get(:life)
+    |> calculate_total_life()
+    |> set_life(player)
+  end
 
-    defp set_life(life, player) when life > 100, do: update_player_life(player, 100)
-    defp set_life(life, player), do: update_player_life(player, life)
+  defp calculate_total_life(life), do: Enum.random(@heal_power) + life
 
-    defp update_player_life(player, life) do
-        player
-        |> Game.fetch_player()
-        |> Map.put(:life, life)
-        |> update_game(player, life)
-    end
+  defp set_life(life, player) when life > 100, do: update_player_life(player, 100)
+  defp set_life(life, player), do: update_player_life(player, life)
 
-    defp update_game(player_data, player, life) do
-        Game.info()
-        |> Map.put(player, player_data)
-        |> Game.update()
+  defp update_player_life(player, life) do
+    player
+    |> Game.fetch_player()
+    |> Map.put(:life, life)
+    |> update_game(player, life)
+  end
 
-        Status.print_move_message(player, :heal, life)
-    end
+  defp update_game(player_data, player, life) do
+    Game.info()
+    |> Map.put(player, player_data)
+    |> Game.update()
+
+    Status.print_move_message(player, :heal, life)
+  end
 end
